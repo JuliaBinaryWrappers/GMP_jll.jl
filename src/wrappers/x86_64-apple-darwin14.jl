a69294cbe95041c8df67d197cb647e185b1f756e
jlbuild <juliabuildbot@gmail.com>
2019-12-11 21:17:18 +0000

    GMP_jll build 6.1.2+4

--- src/wrappers/x86_64-apple-darwin14.jl
+++ src/wrappers/x86_64-apple-darwin14.jl
@@ -24,11 +24,13 @@ const libgmp = "@rpath/libgmp.10.dylib"
 Open all libraries
 """
 function __init__()
-    global prefix = abspath(joinpath(@__DIR__, ".."))
+    global artifact_dir = abspath(artifact"GMP")
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    global libgmp_path = abspath(joinpath(artifact"GMP", libgmp_splitpath...))
+    # We first need to add to LIBPATH_list the libraries provided by Julia
+    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    global libgmp_path = normpath(joinpath(artifact_dir, libgmp_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.
